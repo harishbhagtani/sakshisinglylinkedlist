da9b78eabba235734a653b60e7cdd617dfb83284
harishbhagtani <30958002+harishbhagtani@users.noreply.github.com>
2021-06-20 18:34:22 +0530

    version1

---
+++ cmake-build-debug/CMakeFiles/sakshiSiglyLinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sakshiSiglyLinkedList.dir/main.cpp.o"
+  "sakshiSiglyLinkedList"
+  "sakshiSiglyLinkedList.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sakshiSiglyLinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+Node* createNewNode(int data){
+    Node* newPtr = new Node();
+    newPtr->data = data;
+    newPtr->next = nullptr;
+    return newPtr;
+}
+
+//TODO:Create a function to insert new node at front (Improve)
+void insertNewNodeAtFront(Node*& head, int data){
+    if(head == nullptr){
+        head = createNewNode(data);
+        return;
+    }
+    Node* newPtr = createNewNode(data);
+    newPtr->next = head;
+    head=newPtr;
+}
+
+//TODO: Create a function to traverse and print the element of linked list
+void printLinkedList(Node* head){
+    if(head == nullptr){
+        cout<<"List is empty";
+        return;
+    }
+    Node* temp = head;
+    while(temp != nullptr){
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+}
+
+
+int main() {
+    Node* head = nullptr;
+    insertNewNodeAtFront(head, 5);
+    insertNewNodeAtFront(head, 4);
+    insertNewNodeAtFront(head, 3);
+    insertNewNodeAtFront(head, 2);
+    insertNewNodeAtFront(head, 1);
+    printLinkedList(head);
+    return 0;
+}
